853fa2834b6aa87379305075e4c5bb142e54f0ab
Kautukdesai <kautukra@buffalo.edu>
2017-10-22 13:36:21 -0400

    added kmeans logic. input and output csv needed for project 2 along with the cleaned data given by the instructor

--- project2/main.py
+++ project2/main.py
@@ -1,7 +1,10 @@
-import re
-import pandas as pd
+# import re
+# import os
+
+import math
 import numpy as np
-import os
+import matplotlib.pyplot as plt
+from scipy.cluster.vq import kmeans2
 
 # print the UBIT name and person number
 print('UBitName = ', 'satyasiv')
@@ -10,6 +13,10 @@ print('UBitName = ', 'kautukra')
 print('personNumber = ', 50247648)
 print('\n')  # print section break
 
+"""
+This code was written to clean the data in Querylevelnorm.txt. Now since we have the input data variables X and
+target variable T in the file Querylevelnorm_X and Querylevelnorm_t respectively. We do not use the below code.
+ 
 # variables realted to file
 dataset_filename = 'Querylevelnorm.txt'
 
@@ -29,4 +36,82 @@ with open('cleaned_dataset.txt', 'w') as file:
     file.write(text)
 file.close()
 
-dataset = np.genfromtxt('cleaned_dataset.txt', dtype=None, delimiter=" ")
+letor_dataset = np.genfromtxt('cleaned_dataset.txt', dtype=None, delimiter=" ")
+
+End """
+
+
+def main():
+
+    # input data file does not contain the reference label which is our target. also it does not have the qid (column 2)
+    # basically the file contains the cleaned data. It contains the feature values
+    letor_input = np.genfromtxt('Querylevelnorm_X.csv', delimiter=',')
+
+    # target datafile contains the reference label which is our target value and which determines the relationship
+    # between query and document. Higher the value better the relation.
+    letor_target = np.genfromtxt('Querylevelnorm_t.csv', delimiter=',')
+
+    # print(letor_input[:, 0])  # first column
+
+    "Partition Data Section"
+    data_count = len(letor_input)
+    training_data_row_limit = math.floor(0.8 * data_count)
+    validation_data_row_limit = training_data_row_limit + math.floor(0.1 * data_count)
+
+    training_input = letor_input[:training_data_row_limit]
+    training_target = letor_target[:training_data_row_limit]
+
+    validation_input = letor_input[training_data_row_limit: validation_data_row_limit]
+    validation_target = letor_target[training_data_row_limit: validation_data_row_limit]
+
+    test_input = letor_input[validation_data_row_limit: data_count]
+    test_target = letor_target[validation_data_row_limit: data_count]
+    "Partition Data Section End"
+
+    " Choose spread section "
+    # plt.hist(training_input[:, 1])
+    # plt.show()
+    # plt.hist(training_input[:, 2])
+    # plt.show()
+
+    # To choose the spread for each of the basis functions the histogram of few features was taken.
+    # We see that most of the points have frequency peaks between 0 and 0.2.
+    # So we choose 0.1 as the spread for each of the basis functions.
+
+    " Choose spread section End "
+
+    k_clusters = 30
+    centroids, label = kmeans2(training_input, k_clusters, minit='points')
+
+    # genearate spread of each data point
+    covariance_matrix = np.cov(training_input, rowvar=False)
+    identity_matrix = np.identity(46)
+
+    covariance_matrix_diag = covariance_matrix * identity_matrix
+
+    basis_funct = compute_design_matrix(training_input, centroids.T, covariance_matrix_diag)
+    print(covariance_matrix_diag)
+
+
+def compute_design_matrix(input_data, cluster_centers, spread):
+
+    input_data = np.reshape(input_data, [len(input_data), len(input_data[0])])
+    cluster_centers = np.reshape(cluster_centers, [len(cluster_centers), len(cluster_centers[0])])
+    print(cluster_centers[0])
+    print(cluster_centers[1])
+    broadcast = np.broadcast(np.array(input_data), np.array(cluster_centers))
+
+    out = np.empty(broadcast.shape)
+    out.flat = [x-u for (x,u) in broadcast]
+
+    basis_func = np.exp(
+        np.sum(
+            np.matmul(input_data - cluster_centers, spread) * (input_data - cluster_centers), axis=2
+        ) / (-2)
+    ).T
+
+    return np.insert(basis_func, 0, 1, axis=1)
+
+
+if __name__ == '__main__':
+    main()
