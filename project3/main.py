e37abf5d13c84a9dd04b789afb8234c3ec6c8d5b
kautuk-desai <kautuk.desai@somaiya.edu>
2017-11-17 14:19:29 -0500

    added function for convolutional neural network.

--- project3/main.py
+++ project3/main.py
@@ -13,11 +13,16 @@ def main():
 
 	x = tf.placeholder(tf.float32, [None, 784])
 
+	
+
 	W = tf.Variable(tf.zeros([784, 10]))
 	b = tf.Variable(tf.zeros([10]))
 
 	y = tf.matmul(x, W) + b
 	y_ = tf.placeholder(tf.float32, [None, 10])
+
+	train_cnn(x, y_)
+
 	cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels = y_, logits=y))
 	# cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_* tf.log(y), reduction_indices=[1]))
 	train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
@@ -46,22 +51,21 @@ def main():
 	usps_test_images = np.ndarray([usps_data_count + 1, 784])
 	image_label = 0
 	usps_test_labels = np.ndarray([usps_data_count + 1, 10])
-	size = (28, 28)
+	required_size = (28, 28)
 
 	for i in range(usps_data_count, 0, -1):
 		file_path = './proj3_images/Test/test_' + "{0:0=4d}".format(i) + '.png'
 		img = Image.open(file_path)
-		img = img.resize((28,28))
+		img = img.resize(required_size)
 
 		# if img.mode != 'RGB':
 		# 	img = img.convert('RGB')
 		# im.save("./temp/test_" + "{0:0=4d}".format(i), "JPEG")
-
 		image = np.asarray(img)
 
 		normalized_data = normalize(image)
 		flattened_vector = normalized_data.flatten()
-		usps_test_images[i] = flattened_vector
+		usps_test_images[i-1] = flattened_vector
 
 		if (per_digit_label_counter == usps_per_digit_data):
 			image_label += 1
@@ -70,7 +74,7 @@ def main():
 		per_digit_label_counter += 1
 		label = np.zeros(10)
 		label[image_label] = 1
-		usps_test_labels[i] = label
+		usps_test_labels[i-1] = label
 		
 
 
@@ -79,17 +83,14 @@ def main():
 	# print(len(usps_test_labels))
 	# print(len(usps_test_labels[0]))
 
-	ip = usps_test_images[1:1501]
+	ip = usps_test_images #[1:1501]
 	# print('ip:',ip)
-	op = usps_test_labels[1:1501]
+	op = usps_test_labels #[1:1501]
 	# print('label: ', op)
 
 	print('predicted_output: ', sess.run(predicted_output, feed_dict = {x: ip, y_: op}))
 	print('expected_output: ', sess.run(expected_output, feed_dict = {x: ip, y_: op}))
 	print('Classification accuracy: ', sess.run(accuracy, feed_dict = {x: ip, y_: op}))
-
-
-
 
 def normalize(data):
 	row_sums = data.sum(axis=1)
@@ -97,6 +98,75 @@ def normalize(data):
 	normalized_data = 1 - norm_matrix
 	normalized_data[normalized_data < 1] = 0
 	return normalized_data
+
+
+def weight_variable(shape):
+  initial = tf.truncated_normal(shape, stddev=0.1)
+  return tf.Variable(initial)
+
+def bias_variable(shape):
+  initial = tf.constant(0.1, shape=shape)
+  return tf.Variable(initial)
+
+
+def conv2d(x, W):
+  return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
+
+def max_pool_2x2(x):
+  return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+
+def train_cnn(x, y_):
+	mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+	W_conv1 = weight_variable([5, 5, 1, 32])
+	b_conv1 = bias_variable([32])
+	x_image = tf.reshape(x, [-1, 28, 28, 1])
+	h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
+	h_pool1 = max_pool_2x2(h_conv1)
+
+	## second layer in convolutional neural network
+	W_conv2 = weight_variable([5, 5, 32, 64])
+	b_conv2 = bias_variable([64])
+	
+	h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
+	h_pool2 = max_pool_2x2(h_conv2)
+
+	W_fc1 = weight_variable([7 * 7 * 64, 1024])
+	b_fc1 = bias_variable([1024])
+	
+	h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
+	h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
+
+	## drop random neurons to avoid overfitting
+	keep_prob = tf.placeholder(tf.float32)
+	h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
+
+	## we now perform the prediction using the basic logistic approach to compute the softmax
+	## and then evaluating the output
+	W_fc2 = weight_variable([1024, 10])
+	b_fc2 = bias_variable([10])
+
+	y_conv = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
+
+
+
+	cross_entropy = tf.reduce_mean( tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y_conv))
+	train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
+	correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
+	accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+with tf.Session() as sess:
+  sess.run(tf.global_variables_initializer())
+  for i in range(20000):
+  	batch = mnist.train.next_batch(50)
+    if (i % 100 == 0):
+    	train_accuracy = accuracy.eval(feed_dict={x: batch[0], y_: batch[1], keep_prob: 1.0})
+    	print('step %d, training accuracy %g' % (i, train_accuracy))
+
+    train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
+
+  
+  print('test accuracy %g' % accuracy.eval(feed_dict={
+      x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
 
 if __name__ == '__main__':
 	main()
