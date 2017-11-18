4fdbc78efa738f5294329054292fa22950425332
kautuk-desai <kautuk.desai@somaiya.edu>
2017-11-18 15:43:47 -0500

    added utilities to compute common operations like model training, reading usps data. refactored the code accordingly to use objects and function from module utilities.

--- project3/main.py
+++ project3/main.py
@@ -1,186 +1,132 @@
 import tensorflow as tf
-from tensorflow.examples.tutorials.mnist import input_data
-
 import matplotlib.image as mpimg
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
-import numpy as np
-from PIL import Image
 import PIL.ImageOps as ImageOps
+from utilities import Utilities
 
 
 def main():
-    mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+	# initialize the utilites class. donwloads mnist data and initializes input variable x,
+	# predicted output label valriable y_
+	num_iterations = 20000
+	batch_size = 50
+	utility_obj = Utilities(num_iterations, batch_size)
+	
+	# Read the USPS data from proj3_images folder and store for further use
+	utility_obj.get_usps_data()
 
-    x = tf.placeholder(tf.float32, [None, 784])
-    W = tf.Variable(tf.zeros([784, 10]))
-    b = tf.Variable(tf.zeros([10]))
+	# create the logistic regression model, train using mnist data and test it using mnist and usps data set
+	logistic_regression(utility_obj)
 
-    y = tf.matmul(x, W) + b
-    y_ = tf.placeholder(tf.float32, [None, 10])
+	# create single layer neural network model, train using mnist and test it using mnist and usps
+	num_neurons = 100
+	single_layer_nn(utility_obj,num_neurons)
 
-    # train_cnn(x, y_)
-
-    single_layer_nn(x, y_, mnist)
-
-    cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y))
-    # cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_* tf.log(y), reduction_indices=[1]))
-    train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
-
-    sess = tf.InteractiveSession()
-    tf.global_variables_initializer().run()
-
-    # this is for mini batch stochastic gradient descent
-    for i in range(1000):
-        x_train_batch, y_train_batch = mnist.train.next_batch(100)
-        sess.run(train_step, feed_dict={x: x_train_batch, y_: y_train_batch})
-
-    predicted_output = tf.argmax(y, 1)
-    expected_output = tf.argmax(y_, 1)
-
-    correct_prediction = tf.equal(predicted_output, expected_output)
-    # correct_prediction = tf.Print(correct_prediction, [correct_prediction])
-
-    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-    accuracy = tf.multiply(accuracy, 100)
-
-    # USPS data
-    usps_data_count = 1500
-    usps_per_digit_data = 150
-    per_digit_label_counter = 0
-    usps_test_images = np.ndarray([usps_data_count + 1, 784])
-    image_label = 0
-    usps_test_labels = np.ndarray([usps_data_count + 1, 10])
-    required_size = (28, 28)
-
-    for i in range(usps_data_count, 0, -1):
-        file_path = './proj3_images/Test/test_' + "{0:0=4d}".format(i) + '.png'
-        img = Image.open(file_path)
-        img = img.resize(required_size)
-
-        image = np.asarray(img)
-        normalized_data = normalize(image)
-        # plt.imsave("./temp/test_" + "{0:0=4d}".format(i) + ".png", normalized_data, cmap=cm.gray)
-        flattened_vector = normalized_data.flatten()
-        usps_test_images[i - 1] = flattened_vector
-
-        if (per_digit_label_counter == usps_per_digit_data):
-            image_label += 1
-            per_digit_label_counter = 0
-
-        per_digit_label_counter += 1
-        label = np.zeros(10)
-        label[image_label] = 1
-        usps_test_labels[i - 1] = label
-
-    # print(len(usps_test_images))
-    # print(usps_test_images[0])
-    # print(len(usps_test_labels))
-    # print(len(usps_test_labels[0]))
-
-    print('predicted_output: ', sess.run(predicted_output, feed_dict={x: usps_test_images, y_: usps_test_labels}))
-    print('expected_output: ', sess.run(expected_output, feed_dict={x: usps_test_images, y_: usps_test_labels}))
-    print('Classification accuracy: ', sess.run(accuracy, feed_dict={x: usps_test_images, y_: usps_test_labels}))
+	# create convolutional neural network model, train using mnist and test it using mnist and usps
+	# train_cnn(utility_obj)
 
 
-def normalize(data):
-    row_sums = data.sum(axis=1)
-    norm_matrix = np.divide(data, row_sums[:,np.newaxis])  # increase the dimension of the existing array by one more dimension
-    normalized_data = 1 - norm_matrix
-    normalized_data[normalized_data < 1] = 0
-    return normalized_data
+def logistic_regression(utility_obj):
+	W = tf.Variable(tf.zeros([784, 10]))  # weights
+	b = tf.Variable(tf.zeros([10])) # bias
 
-def single_layer_nn(x, y_, mnist):
-	hidden_layer = tf.layers.dense(x, 100, activation=tf.nn.relu)
-	h_1 = tf.layers.dense(hidden_layer, 10)
+	y = tf.matmul(utility_obj.x_input, W) + b # logistic regression model
+	print('########## Logistic Regression Model Training ##########')
+	logistic_reg_accuracy, logistic_reg_sess = utility_obj.compute_model_training(y)
 
-	cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=h_1))
-	train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
-	correct_prediction = tf.equal(tf.argmax(h_1, 1), tf.argmax(y_, 1))
-	accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+	print('MNIST Logistic Regression Accuracy =', logistic_reg_sess.run(
+		logistic_reg_accuracy, feed_dict={utility_obj.x_input: utility_obj.mnist.test.images, utility_obj.y_labels: utility_obj.mnist.test.labels}))
 
-	print('######## Sinle layer NN training ########')
-	with tf.Session() as snn_sess:
-		snn_sess.run(tf.global_variables_initializer())
-		for i in range(20000):
-			batch = mnist.train.next_batch(50)
-			if (i % 1000 == 0):
-				train_accuracy = accuracy.eval(feed_dict={x: batch[0], y_: batch[1]})
-				print('step %d, training accuracy %g' % (i, train_accuracy))
+	print('USPS Logistic Regression Accuracy =', logistic_reg_sess.run(
+		logistic_reg_accuracy, feed_dict={utility_obj.x_input: utility_obj.usps_test_images, utility_obj.y_labels: utility_obj.usps_test_labels}))
+	logistic_reg_sess.close()
 
-			train_step.run(feed_dict={x: batch[0], y_: batch[1]})
+def single_layer_nn(utility_obj, num_neurons):
+	hidden_layer = tf.layers.dense(utility_obj.x_input, num_neurons, activation=tf.nn.relu)
+	h_1 = tf.layers.dense(hidden_layer, 10) # number of classes in which the data is to be classified
 
-		print('Single Layer MNIST Accuracy %g' % accuracy.eval(feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
+	print('########## Sinle layer NN training ##########')
+	snn_accuracy, snn_sess = utility_obj.compute_model_training(h_1)
 
+	print('Single Layer NN MNIST Accuracy = ', snn_sess.run(
+		snn_accuracy, feed_dict={utility_obj.x_input: utility_obj.mnist.test.images, utility_obj.y_labels: utility_obj.mnist.test.labels}))
+
+	print('USPS Single Layer NN Accuracy =', snn_sess.run(
+		snn_accuracy, feed_dict={utility_obj.x_input: utility_obj.usps_test_images, utility_obj.y_labels: utility_obj.usps_test_labels}))
+	snn_sess.close()
 
 
 def weight_variable(shape):
-    initial = tf.truncated_normal(shape, stddev=0.1)
-    return tf.Variable(initial)
+	initial = tf.truncated_normal(shape, stddev=0.1)
+	return tf.Variable(initial)
 
 
 def bias_variable(shape):
-    initial = tf.constant(0.1, shape=shape)
-    return tf.Variable(initial)
+	initial = tf.constant(0.1, shape=shape)
+	return tf.Variable(initial)
 
 
 def conv2d(x, W):
-    return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
+	return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
 
 
 def max_pool_2x2(x):
-    return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+	return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
 
 
 def train_cnn(x, y_):
-    mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
-    W_conv1 = weight_variable([5, 5, 1, 32])
-    b_conv1 = bias_variable([32])
-    x_image = tf.reshape(x, [-1, 28, 28, 1])
-    h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
-    h_pool1 = max_pool_2x2(h_conv1)
+	mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+	W_conv1 = weight_variable([5, 5, 1, 32])
+	b_conv1 = bias_variable([32])
+	x_image = tf.reshape(x, [-1, 28, 28, 1])
+	h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
+	h_pool1 = max_pool_2x2(h_conv1)
 
-    ## second layer in convolutional neural network
-    W_conv2 = weight_variable([5, 5, 32, 64])
-    b_conv2 = bias_variable([64])
+	# second layer in convolutional neural network
+	W_conv2 = weight_variable([5, 5, 32, 64])
+	b_conv2 = bias_variable([64])
 
-    h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
-    h_pool2 = max_pool_2x2(h_conv2)
+	h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
+	h_pool2 = max_pool_2x2(h_conv2)
 
-    W_fc1 = weight_variable([7 * 7 * 64, 1024])
-    b_fc1 = bias_variable([1024])
+	W_fc1 = weight_variable([7 * 7 * 64, 1024])
+	b_fc1 = bias_variable([1024])
 
-    h_pool2_flat = tf.reshape(h_pool2, [-1, 7 * 7 * 64])
-    h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
+	h_pool2_flat = tf.reshape(h_pool2, [-1, 7 * 7 * 64])
+	h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
 
-    ## drop random neurons to avoid overfitting
-    keep_prob = tf.placeholder(tf.float32)
-    h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
+	# drop random neurons to avoid overfitting
+	keep_prob = tf.placeholder(tf.float32)
+	h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
 
-    ## we now perform the prediction using the basic logistic approach to compute the softmax
-    ## and then evaluating the output
-    W_fc2 = weight_variable([1024, 10])
-    b_fc2 = bias_variable([10])
+	# we now perform the prediction using the basic logistic approach to compute the softmax
+	# and then evaluating the output
+	W_fc2 = weight_variable([1024, 10])
+	b_fc2 = bias_variable([10])
 
-    y_conv = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
+	y_conv = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
 
-    cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y_conv))
-    train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
-    correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
-    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+	cross_entropy = tf.reduce_mean(
+		tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y_conv))
+	train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
+	correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
+	accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
-    with tf.Session() as cnn_sess:
-        cnn_sess.run(tf.global_variables_initializer())
-        for i in range(20000):
-            batch = mnist.train.next_batch(50)
-            if (i % 100 == 0):
-                train_accuracy = accuracy.eval(feed_dict={x: batch[0], y_: batch[1], keep_prob: 1.0})
-                print('step %d, training accuracy %g' % (i, train_accuracy))
+	with tf.Session() as cnn_sess:
+		cnn_sess.run(tf.global_variables_initializer())
+		for i in range(20000):
+			batch = mnist.train.next_batch(50)
+			if (i % 100 == 0):
+				train_accuracy = accuracy.eval(
+					feed_dict={x: batch[0], y_: batch[1], keep_prob: 1.0})
+				print('step %d, training accuracy %g' % (i, train_accuracy))
 
-            train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
+			train_step.run(
+				feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
 
-        print('test accuracy %g' % accuracy.eval(feed_dict={x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
+		print('test accuracy %g' % accuracy.eval(feed_dict={x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
 
 
 if __name__ == '__main__':
-    main()
+	main()
---
+++ project3/utilities.py
@@ -0,0 +1,76 @@
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+import numpy as np
+from PIL import Image
+
+
+class Utilities:
+
+	def __init__(self, num_iterations, batch_size):
+		self.num_iterations = num_iterations
+		self.batch_size = batch_size
+		self.mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+		self.y_labels = tf.placeholder(tf.float32, [None, 10])
+		self.x_input = tf.placeholder(tf.float32, [None, 784]) # input images in vector shape of 784
+
+	def normalize(self, data):
+		row_sums = data.sum(axis=1)
+		# increase the dimension of the existing array by one more dimension
+		norm_matrix = np.divide(data, row_sums[:, np.newaxis])
+		normalized_data = 1 - norm_matrix
+		normalized_data[normalized_data < 1] = 0
+		return normalized_data
+
+	def get_usps_data(self):
+		usps_data_count = 1500
+		usps_per_digit_data = 150
+		per_digit_label_counter = 0
+		usps_test_images = np.ndarray([usps_data_count + 1, 784])
+		image_label = 0
+		usps_test_labels = np.ndarray([usps_data_count + 1, 10])
+		required_size = (28, 28)
+
+		for i in range(usps_data_count, 0, -1):
+			file_path = './proj3_images/Test/test_' + "{0:0=4d}".format(i) + '.png'
+			img = Image.open(file_path)
+			img = img.resize(required_size)
+
+			image = np.asarray(img)
+			normalized_data = self.normalize(image)
+			# plt.imsave("./temp/test_" + "{0:0=4d}".format(i) + ".png", normalized_data, cmap=cm.gray)
+			flattened_vector = normalized_data.flatten()
+			usps_test_images[i - 1] = flattened_vector
+
+			if (per_digit_label_counter == usps_per_digit_data):
+				image_label += 1
+				per_digit_label_counter = 0
+
+			per_digit_label_counter += 1
+			label = np.zeros(10)
+			label[image_label] = 1
+			usps_test_labels[i - 1] = label
+
+		self.usps_test_images = usps_test_images
+		self.usps_test_labels = usps_test_labels
+
+	def compute_model_training(self, prediction):
+		cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(
+			labels=self.y_labels, logits=prediction))
+		train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+
+		correct_prediction = tf.equal(tf.argmax(prediction, 1), tf.argmax(self.y_labels, 1))
+		accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+		accuracy = tf.multiply(accuracy, 100)
+	
+		sess = tf.InteractiveSession()
+		tf.global_variables_initializer().run()
+		# this is for mini batch stochastic gradient descent
+		for i in range(self.num_iterations):
+			batch = self.mnist.train.next_batch(self.batch_size)
+			if (i % 5000 == 0):
+				train_accuracy = accuracy.eval(feed_dict={self.x_input: batch[0], self.y_labels: batch[1]})
+				print('step %d, training accuracy %g' % (i, train_accuracy))
+	
+			train_step.run(feed_dict={self.x_input: batch[0], self.y_labels: batch[1]})
+	
+		return accuracy, sess
