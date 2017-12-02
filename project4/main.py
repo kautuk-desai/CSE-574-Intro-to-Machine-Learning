59e60b9c8f9fca3ffd20ca97e5bd895addc2b7af
kautuk-desai <kautuk.desai@somaiya.edu>
2017-12-02 14:06:26 -0500

    added neural network function

--- project4/main.py
+++ project4/main.py
@@ -1,6 +1,7 @@
 import numpy as np
 import random
 from PIL import Image
+import tensorflow as tf
 from groupmembers import print_group_members
 from utilities import Utilities
 
@@ -13,10 +14,10 @@ def main():
 	## read label file
 	label_file_name = './data/list_attr_celeba.txt'
 	features = np.genfromtxt(label_file_name, skip_header=1, max_rows=1, dtype='str')
-	print(len(features))
+	# print(len(features))
 	# bad method but couldn't find any alternative
 	feature_col_index = np.where(features == feature_for_classification)[0][0]
-	print(feature_col_index)
+	print('Eyeglass column index: ', feature_col_index)
 	
 	label = np.genfromtxt(label_file_name, dtype= 'str',skip_header=2, usecols=(0, feature_col_index))
 	dataset_count = len(label)
@@ -43,8 +44,54 @@ def main():
 	celeba_train_images = utilities.load_images(training_count,celeba_train_img_file_names)
 	#celeba_test_images = utilities.load_images(1000, celeba_test_img_file_names)
 
-	print(celeba_train_images[0])
+	print(len(celeba_train_images))
+
+	train_cnn(utilities, celeba_train_images, celeba_train_labels)
+
+
+def train_cnn(utility_obj, training_data, training_label):
+	W_conv1 = utility_obj.weight_variable([5, 5, 1, 32])
+	b_conv1 = utility_obj.bias_variable([32])
+	x_image = tf.reshape(utility_obj.x_input, [-1, 28, 28, 1])
+	h_conv1 = tf.nn.relu(utility_obj.conv2d(x_image, W_conv1) + b_conv1)
+	h_pool1 = utility_obj.max_pool_2x2(h_conv1)
+
+	# second layer in convolutional neural network
+	W_conv2 = utility_obj.weight_variable([5, 5, 32, 64])
+	b_conv2 = utility_obj.bias_variable([64])
+
+	h_conv2 = tf.nn.relu(utility_obj.conv2d(h_pool1, W_conv2) + b_conv2)
+	h_pool2 = utility_obj.max_pool_2x2(h_conv2)
+
+	W_fc1 = utility_obj.weight_variable([7 * 7 * 64, 1024])
+	b_fc1 = utility_obj.bias_variable([1024])
+
+	h_pool2_flat = tf.reshape(h_pool2, [-1, 7 * 7 * 64])
+	h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
+
+	# drop random neurons to avoid overfitting
 	
+	h_fc1_drop = tf.nn.dropout(h_fc1, utility_obj.keep_prob)
+
+	# we now perform the prediction using the basic logistic approach to compute the softmax
+	# and then evaluating the output
+	W_fc2 = utility_obj.weight_variable([1024, 1])
+	b_fc2 = utility_obj.bias_variable([1])
+
+	y_conv = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
+
+	cnn_accuracy, cnn_sess = utility_obj.compute_model_training(y_conv, training_data, training_label)
+
+	## Testing the model
+	print('CNN MNIST Accuracy = ', cnn_sess.run(cnn_accuracy,
+		feed_dict={utility_obj.x_input: utility_obj.mnist.test.images,
+		utility_obj.y_labels: utility_obj.mnist.test.labels, utility_obj.keep_prob: 1.0}))
+
+	print('CNN USPS Accuracy = ', cnn_sess.run(cnn_accuracy,
+		feed_dict={utility_obj.x_input: utility_obj.usps_test_images,
+		utility_obj.y_labels: utility_obj.usps_test_labels, utility_obj.keep_prob: 1.0}))
+
+	cnn_sess.close()
 
 if __name__ == '__main__':
 	main()
--- project4/utilities.py
+++ project4/utilities.py
@@ -1,22 +1,68 @@
 from PIL import Image
 import numpy as np
+import tensorflow as tf
+
 
 class Utilities(object):
-	"""docstring for ClassName"""
-	def __init__(self, file_path):
-		super(Utilities, self).__init__()
-		self.file_path = file_path
-		self.image_size = (28,28)
+    """docstring for ClassName"""
 
-	def load_images(self, num_images, file_names):
-		data = [[]]*num_images
-		for i in range(num_images):
-			im = Image.open(self.file_path + file_names[i])
-			im = im.convert(mode='L')
-			resized_im = im.resize(self.image_size)
-			# resized_im.show()
-			flattened_im = np.asarray(resized_im).flatten()
-			data[i] = flattened_im
+    def __init__(self, file_path):
+        super(Utilities, self).__init__()
+        self.file_path = file_path
+        self.image_size = (28, 28)
+        self.x_input = tf.placeholder(tf.float32, [None, 784])  # input images in vector shape of 784
+        self.y_labels = tf.placeholder(tf.float32, [None])
+        self.keep_prob = tf.placeholder(tf.float32)  # used for cnn neurons droping probability
+        self.num_iterations = 300
+        self.batch_size = 5000
+        self.learning_rate = 1e-4
+        self.optimizer = tf.train.AdamOptimizer(self.learning_rate)
 
-		return data
-		
+    def load_images(self, num_images, file_names):
+        data = [[]] * num_images
+        for i in range(num_images):
+            im = Image.open(self.file_path + file_names[i])
+            im = im.convert(mode='L')
+            resized_im = im.resize(self.image_size)
+            # resized_im.show()
+            flattened_im = np.asarray(resized_im).flatten()
+            data[i] = flattened_im
+
+        return data
+
+    def compute_model_training(self, prediction, training_data, training_label):
+        cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(
+            labels=self.y_labels, logits=prediction))
+        train_step = self.optimizer.minimize(cross_entropy)
+
+        correct_prediction = tf.equal(tf.argmax(prediction, 1), tf.argmax(self.y_labels, 1))
+        accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+        accuracy = tf.multiply(accuracy, 100)
+
+        sess = tf.InteractiveSession()
+        tf.global_variables_initializer().run()
+        # this is for mini batch stochastic gradient descent
+        for i in range(self.num_iterations):
+            input_batch = training_data[i*self.batch_size : (i+1)*self.batch_size]
+            output_label = training_label[i*self.batch_size : (i+1)*self.batch_size]
+            if (i % 5000 == 0):
+                train_accuracy = accuracy.eval(feed_dict={self.x_input: input_batch, self.y_labels: output_label, self.keep_prob: 0.5})
+                print('step %d, training accuracy %g' % (i, train_accuracy))
+
+            train_step.run(feed_dict={self.x_input: input_batch, self.y_labels: output_label, self.keep_prob: 0.5})
+
+        return accuracy, sess
+
+    def weight_variable(self, shape):
+        initial = tf.truncated_normal(shape, stddev=0.1)
+        return tf.Variable(initial)
+
+    def bias_variable(self, shape):
+        initial = tf.constant(0.1, shape=shape)
+        return tf.Variable(initial)
+
+    def conv2d(self, x, W):
+        return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
+
+    def max_pool_2x2(self, x):
+        return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
