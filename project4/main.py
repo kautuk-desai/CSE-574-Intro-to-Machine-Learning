3780b036d073eae628a18316d096ec80f6cfc57b
kautuk-desai <kautuk.desai@somaiya.edu>
2017-12-06 13:06:21 -0500

    added logic for data augmentation. Just rotated the images by 90

--- project4/main.py
+++ project4/main.py
@@ -4,32 +4,38 @@ import tensorflow as tf
 from groupmembers import print_group_members
 from utilities import Utilities
 
+
 def main():
 	print_group_members()
 
 	feature_for_classification = 'Eyeglasses'
 	data_file_path = './data/img_align_celeba/'
 
-	## read label file
+	# read label file
 	label_file_name = './data/list_attr_celeba.txt'
 	features = np.genfromtxt(label_file_name, skip_header=1, max_rows=1, dtype='str')
 	# print(len(features))
 	# bad method but couldn't find any alternative
 	feature_col_index = np.where(features == feature_for_classification)[0][0]
 	print('Eyeglass column index: ', feature_col_index)
-	
-	label = np.genfromtxt(label_file_name, dtype= 'str',skip_header=2, usecols=(0, feature_col_index))
+
+	label = np.genfromtxt(label_file_name, dtype='str', skip_header=2, usecols=(0, feature_col_index))
 	dataset_count = len(label)
 	training_count = int(0.8 * dataset_count)
 	test_count = int(0.2 * dataset_count)
 	small_training_count = int(0.01 * training_count)
 	# python's random module isn’t made to deal with numpy arrays
 	# since it’s not exactly the same as nested python lists
+	np.random.seed(20)
 	np.random.shuffle(label)
 	image_file_names = label[:, 0]
 	expected_output = label[:, 1]
 	b = expected_output.astype(np.int).clip(min=0)
 	one_hot_outputs = np.eye(2)[b]
+
+	print('augmenting data..')
+	utilities.augmentation_count = 10
+	augmented_imgs, augmented_labels = utilities.data_augmentation(label)
 	# print(image_file_names[0])
 	# print(expected_output[0])
 	# print(b[0])
@@ -38,45 +44,48 @@ def main():
 	# a.show()
 
 	celeba_train_img_file_names = image_file_names[0:small_training_count]
-	celeba_test_img_file_names = image_file_names[training_count:dataset_count]
-	# print('train image count: ', len(celeba_train_img_file_names))
-	# print('test image count: ', len(celeba_test_img_file_names))
-
 	celeba_train_labels = one_hot_outputs[0:small_training_count]
+
+	celeba_test_img_file_names = image_file_names[training_count:dataset_count]
 	celeba_test_labels = one_hot_outputs[training_count:dataset_count]
-	print('Num train labels: ', len(celeba_train_labels))
-	print('Num test labels: ', len(celeba_test_labels))
 
 	utilities = Utilities(data_file_path)
-	celeba_train_images = utilities.load_images(small_training_count,celeba_train_img_file_names)
-	celeba_test_images = utilities.load_images(100, celeba_test_img_file_names)
+	celeba_train_images = utilities.load_images(small_training_count, celeba_train_img_file_names)
+	celeba_train_images = np.append(celeba_train_images, augmented_imgs, axis=0)
+	celeba_train_labels = np.append(celeba_train_labels, augmented_labels, axis=0)
+
+	celeba_test_images = utilities.load_images(test_count, celeba_test_img_file_names)
+
+	print('Num train data: ', len(celeba_train_images))
+	print('Num test data: ', len(celeba_test_images))
 
 	print('Model training started...')
-	train_cnn(utilities, celeba_train_images, celeba_train_labels,celeba_test_images,celeba_test_labels)
+	train_cnn(utilities, celeba_train_images, celeba_train_labels, celeba_test_images, celeba_test_labels)
 
 
-def train_cnn(utility_obj, training_data, training_label,celeba_test_images,celeba_test_labels):
-	W_conv1 = utility_obj.weight_variable([5, 5, 1, 60])
-	b_conv1 = utility_obj.bias_variable([60])
-	x_image = tf.reshape(utility_obj.x_input, [-1, 56, 56, 1])
+def train_cnn(utility_obj, training_data, training_label, celeba_test_images, celeba_test_labels):
+
+	W_conv1 = utility_obj.weight_variable([5, 5, 1, utility_obj.nodes_layer_1])
+	b_conv1 = utility_obj.bias_variable([utility_obj.nodes_layer_1])
+	x_image = tf.reshape(utility_obj.x_input, [-1, utility_obj.image_height, utility_obj.image_height, 1])
 	h_conv1 = tf.nn.relu(utility_obj.conv2d(x_image, W_conv1) + b_conv1)
 	h_pool1 = utility_obj.max_pool_2x2(h_conv1)
 
 	# second layer in convolutional neural network
-	W_conv2 = utility_obj.weight_variable([5, 5, 60, 120])
-	b_conv2 = utility_obj.bias_variable([120])
+	W_conv2 = utility_obj.weight_variable([5, 5, utility_obj.nodes_layer_1, utility_obj.nodes_layer_2])
+	b_conv2 = utility_obj.bias_variable([utility_obj.nodes_layer_2])
 
 	h_conv2 = tf.nn.relu(utility_obj.conv2d(h_pool1, W_conv2) + b_conv2)
 	h_pool2 = utility_obj.max_pool_2x2(h_conv2)
 
-	W_fc1 = utility_obj.weight_variable([14 * 14 * 120, 1024])
+	W_fc1 = utility_obj.weight_variable([(utility_obj.image_height // 4) * (utility_obj.image_height // 4) * utility_obj.nodes_layer_2, 1024])
 	b_fc1 = utility_obj.bias_variable([1024])
 
-	h_pool2_flat = tf.reshape(h_pool2, [-1, 14 * 14 * 120])
+	h_pool2_flat = tf.reshape(h_pool2, [-1, (utility_obj.image_height // 4) * (utility_obj.image_height // 4) * utility_obj.nodes_layer_2])
 	h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
 
 	# drop random neurons to avoid overfitting
-	
+
 	h_fc1_drop = tf.nn.dropout(h_fc1, utility_obj.keep_prob)
 
 	# we now perform the prediction using the basic logistic approach to compute the softmax
@@ -88,39 +97,36 @@ def train_cnn(utility_obj, training_data, training_label,celeba_test_images,cele
 
 	cnn_accuracy, cnn_sess = utility_obj.compute_model_training(y_conv, training_data, training_label)
 
-
-	## just for fun we asked the model whether it can correctly classify group members
+	# just for fun we asked the model whether it can correctly classify group members
 	# group_members_img = ['kautuk_desai.jpg','ub_directory.jpg']
 	# group_members_labels = np.eye(2)[np.transpose([1,1])]
-	
+
 	# for i in range(len(group_members_img)):
 	# 	im = Image.open('./data/group_members/' + group_members_img[i])
 	# 	im = im.convert(mode='L')
 	# 	resized_im = im.resize(utility_obj.image_size)
 	# 	# resized_im.show()
 	# 	flattened_im = np.asarray(resized_im).flatten()
-		
+
 	# 	group_member = cnn_sess.run(cnn_accuracy,feed_dict={utility_obj.x_input: [flattened_im],
 	# 		utility_obj.y_labels: [group_members_labels[i]], utility_obj.keep_prob: 1.0})
 
 	# 	print('Classified ',group_members_img[i],' with Accuracy = ', group_member)
 
-	test_batch_size = 1000
+	test_batch_size = 100
 	test_data_size = len(celeba_test_labels)
 	testing_batch_iterations = test_data_size // test_batch_size
 	test_accuracy = 0.0
-	
+	print('Testing now..')
 	for i in range(testing_batch_iterations):
-		test_images_batch = celeba_test_images[i*test_batch_size : min((i+1)*test_batch_size, test_data_size)]
-		target_labels_batch = celeba_test_labels[i*test_batch_size : min((i+1)*test_batch_size, test_data_size)]
-		batch_test_accuracy = cnn_sess.run(cnn_accuracy,feed_dict={utility_obj.x_input: test_images_batch,
-		utility_obj.y_labels: target_labels_batch, utility_obj.keep_prob: 1.0})
+		test_images_batch = celeba_test_images[i * test_batch_size: min((i + 1) * test_batch_size, test_data_size)]
+		target_labels_batch = celeba_test_labels[i * test_batch_size: min((i + 1) * test_batch_size, test_data_size)]
+		batch_test_accuracy = cnn_sess.run(cnn_accuracy, feed_dict={utility_obj.x_input: test_images_batch,
+																	utility_obj.y_labels: target_labels_batch, utility_obj.keep_prob: 1.0})
 
-		test_accuracy = test_accuracy+batch_test_accuracy
-		print('Testing batch = ',i,' Accuracy = ', batch_test_accuracy)
-	print('Test data Accuracy = ', test_accuracy/testing_batch_iterations)
-
-	
+		test_accuracy = test_accuracy + batch_test_accuracy
+		#print('Testing batch = ',i,' Accuracy = ', batch_test_accuracy)
+	print('Test data Accuracy = ', test_accuracy / testing_batch_iterations)
 
 	cnn_sess.close()
 
--- project4/utilities.py
+++ project4/utilities.py
@@ -4,71 +4,92 @@ import tensorflow as tf
 
 
 class Utilities(object):
-    """docstring for ClassName"""
+	"""docstring for ClassName"""
 
-    def __init__(self, file_path):
-        super(Utilities, self).__init__()
-        self.file_path = file_path
-        self.image_size = (56, 56)
-        self.x_input = tf.placeholder(tf.float32, [None, 3136])  # input images in vector shape of 784
-        self.y_labels = tf.placeholder(tf.float32, [None,2])
-        self.keep_prob = tf.placeholder(tf.float32)  # used for cnn neurons droping probability
-        self.batch_size = 1000
-        self.learning_rate = 1e-3
-        self.optimizer = tf.train.AdamOptimizer(self.learning_rate)
-        self.num_epochs = 10
+	def __init__(self, file_path):
+		super(Utilities, self).__init__()
+		self.file_path = file_path
+		self.image_height = 28
+		self.image_size = (self.image_height, self.image_height)
+		self.x_input = tf.placeholder(tf.float32, [None, self.image_height * self.image_height])  # input images in vector shape of 784
+		self.y_labels = tf.placeholder(tf.float32, [None, 2])
+		self.keep_prob = tf.placeholder(tf.float32)  # used for cnn neurons droping probability
+		self.batch_size = 100
+		self.learning_rate = 1e-3
+		self.optimizer = tf.train.AdamOptimizer(self.learning_rate)
+		self.num_epochs = 25
+		self.keep_prob_value = 0.6
+		self.nodes_layer_1 = 64
+		self.nodes_layer_2 = 32
+		self.augment_data = False
 
-    def load_images(self, num_images, file_names):
-        data = [[]] * num_images
-        for i in range(num_images):
-            im = Image.open(self.file_path + file_names[i])
-            im = im.convert(mode='L')
-            resized_im = im.resize(self.image_size)
-            # resized_im.show()
-            flattened_im = np.asarray(resized_im).flatten()
-            data[i] = flattened_im
+	def load_images(self, num_images, file_names):
+		data = [[]] * num_images
+		for i in range(num_images):
+			im = Image.open(self.file_path + file_names[i])
+			im = im.convert(mode='L')
+			if (self.augment_data):
+				im = im.rotate(90)
+				im.show()
+			resized_im = im.resize(self.image_size)
+			# resized_im.show()
+			flattened_im = np.asarray(resized_im).flatten()
+			data[i] = flattened_im
 
-        return data
+		return data
 
-    def compute_model_training(self, prediction, training_data, training_label):
-        cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(
-            labels=self.y_labels, logits=prediction))
-        train_step = self.optimizer.minimize(cross_entropy)
-        correct_prediction = tf.equal(tf.argmax(prediction, 1), tf.argmax(self.y_labels, 1))
-        accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-        accuracy = tf.multiply(accuracy, 100)
+	def compute_model_training(self, prediction, training_data, training_label):
+		cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(
+			labels=self.y_labels, logits=prediction))
+		train_step = self.optimizer.minimize(cross_entropy)
+		correct_prediction = tf.equal(tf.argmax(prediction, 1), tf.argmax(self.y_labels, 1))
+		accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+		accuracy = tf.multiply(accuracy, 100)
 
-        sess = tf.InteractiveSession()
-        tf.global_variables_initializer().run()
-        # this is for mini batch stochastic gradient descent
-        training_data_count = len(training_data)
-        num_iterations =  training_data_count // self.batch_size
-        input_batch = []
-        output_label = []
-        for j in range(self.num_epochs):
-            print('Epoch %d started...' %j)
-            for i in range(num_iterations):
-                input_batch = training_data[i*self.batch_size : min((i+1)*self.batch_size, training_data_count)]
-                output_label = training_label[i*self.batch_size : min((i+1)*self.batch_size, training_data_count)]
-                # train_accuracy = accuracy.eval(feed_dict={self.x_input: input_batch, self.y_labels: output_label, self.keep_prob: 0.5})
-                # print('step %d, training accuracy %g' % (i, train_accuracy))
-                train_step.run(feed_dict={self.x_input: input_batch, self.y_labels: output_label, self.keep_prob: 0.6})
+		sess = tf.InteractiveSession()
+		tf.global_variables_initializer().run()
+		# this is for mini batch stochastic gradient descent
+		training_data_count = len(training_data)
+		num_iterations = training_data_count // self.batch_size
+		input_batch = []
+		output_label = []
+		for j in range(self.num_epochs):
+			print('Epoch %d started...' % j)
+			train_accuracy = 0
+			for i in range(num_iterations):
+				input_batch = training_data[i * self.batch_size: min((i + 1) * self.batch_size, training_data_count)]
+				output_label = training_label[i * self.batch_size: min((i + 1) * self.batch_size, training_data_count)]
+				# train_accuracy = accuracy.eval(feed_dict={self.x_input: input_batch, self.y_labels: output_label, self.keep_prob: 0.5})
+				# print('step %d, training accuracy %g' % (i, train_accuracy))
+				train_accuracy = train_accuracy + accuracy.eval(feed_dict={self.x_input: input_batch, self.y_labels: output_label, self.keep_prob: 1.0})
+				train_step.run(feed_dict={self.x_input: input_batch, self.y_labels: output_label, self.keep_prob: self.keep_prob_value})
 
-            train_accuracy = accuracy.eval(feed_dict={self.x_input: input_batch, self.y_labels: output_label, self.keep_prob: 0.6})
-            print('Epoch %d completed. Accuracy: %0.2f' %(j, train_accuracy))
+			#train_accuracy = train_accuracy/num_iterations
+			print('Epoch %d completed. Accuracy: %0.2f' % (j, train_accuracy / num_iterations))
 
-        return accuracy, sess
+		return accuracy, sess
 
-    def weight_variable(self, shape):
-        initial = tf.truncated_normal(shape, stddev=0.1)
-        return tf.Variable(initial)
+	def weight_variable(self, shape):
+		initial = tf.truncated_normal(shape, stddev=0.1)
+		return tf.Variable(initial)
 
-    def bias_variable(self, shape):
-        initial = tf.constant(0.1, shape=shape)
-        return tf.Variable(initial)
+	def bias_variable(self, shape):
+		initial = tf.constant(0.1, shape=shape)
+		return tf.Variable(initial)
 
-    def conv2d(self, x, W):
-        return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
+	def conv2d(self, x, W):
+		return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
 
-    def max_pool_2x2(self, x):
-        return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+	def max_pool_2x2(self, x):
+		return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+
+	def data_augmentation(self, label):
+		self.augment_data = True
+		image_file_names = label[0:self.augmentation_count, 0]
+		expected_output = label[0:self.augmentation_count, 1]
+
+		augmented_imgs = self.load_images(self.augmentation_count, image_file_names)
+		b = expected_output.astype(np.int).clip(min=0)
+		augmented_labels = np.eye(2)[b]
+
+		return augmented_imgs, augmented_labels
