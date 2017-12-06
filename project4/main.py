303a6965be647cb503020cc9e3fc6053aef1e1d4
kautuk-desai <kautuk.desai@somaiya.edu>
2017-12-05 23:50:50 -0500

    resolved issue with 56x56 image size.

--- project4/main.py
+++ project4/main.py
@@ -22,7 +22,7 @@ def main():
 	dataset_count = len(label)
 	training_count = int(0.8 * dataset_count)
 	test_count = int(0.2 * dataset_count)
-	# small_training_count = int(0.5 * training_count)
+	small_training_count = int(0.01 * training_count)
 	# python's random module isn’t made to deal with numpy arrays
 	# since it’s not exactly the same as nested python lists
 	np.random.shuffle(label)
@@ -37,42 +37,42 @@ def main():
 	# a = Image.open('./data/img_align_celeba/' + image_file_names[0])
 	# a.show()
 
-	celeba_train_img_file_names = image_file_names[0:training_count]
+	celeba_train_img_file_names = image_file_names[0:small_training_count]
 	celeba_test_img_file_names = image_file_names[training_count:dataset_count]
 	# print('train image count: ', len(celeba_train_img_file_names))
 	# print('test image count: ', len(celeba_test_img_file_names))
 
-	celeba_train_labels = one_hot_outputs[0:training_count]
+	celeba_train_labels = one_hot_outputs[0:small_training_count]
 	celeba_test_labels = one_hot_outputs[training_count:dataset_count]
 	print('Num train labels: ', len(celeba_train_labels))
 	print('Num test labels: ', len(celeba_test_labels))
 
 	utilities = Utilities(data_file_path)
-	celeba_train_images = utilities.load_images(training_count,celeba_train_img_file_names)
-	celeba_test_images = utilities.load_images(test_count, celeba_test_img_file_names)
+	celeba_train_images = utilities.load_images(small_training_count,celeba_train_img_file_names)
+	celeba_test_images = utilities.load_images(100, celeba_test_img_file_names)
 
 	print('Model training started...')
 	train_cnn(utilities, celeba_train_images, celeba_train_labels,celeba_test_images,celeba_test_labels)
 
 
 def train_cnn(utility_obj, training_data, training_label,celeba_test_images,celeba_test_labels):
-	W_conv1 = utility_obj.weight_variable([5, 5, 1, 32])
-	b_conv1 = utility_obj.bias_variable([32])
+	W_conv1 = utility_obj.weight_variable([5, 5, 1, 60])
+	b_conv1 = utility_obj.bias_variable([60])
 	x_image = tf.reshape(utility_obj.x_input, [-1, 56, 56, 1])
 	h_conv1 = tf.nn.relu(utility_obj.conv2d(x_image, W_conv1) + b_conv1)
 	h_pool1 = utility_obj.max_pool_2x2(h_conv1)
 
 	# second layer in convolutional neural network
-	W_conv2 = utility_obj.weight_variable([5, 5, 32, 64])
-	b_conv2 = utility_obj.bias_variable([64])
+	W_conv2 = utility_obj.weight_variable([5, 5, 60, 120])
+	b_conv2 = utility_obj.bias_variable([120])
 
 	h_conv2 = tf.nn.relu(utility_obj.conv2d(h_pool1, W_conv2) + b_conv2)
 	h_pool2 = utility_obj.max_pool_2x2(h_conv2)
 
-	W_fc1 = utility_obj.weight_variable([7 * 7 * 64, 1024])
+	W_fc1 = utility_obj.weight_variable([14 * 14 * 120, 1024])
 	b_fc1 = utility_obj.bias_variable([1024])
 
-	h_pool2_flat = tf.reshape(h_pool2, [-1, 7 * 7 * 64])
+	h_pool2_flat = tf.reshape(h_pool2, [-1, 14 * 14 * 120])
 	h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
 
 	# drop random neurons to avoid overfitting
